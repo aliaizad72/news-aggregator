d07e65d1acd60f91d064bb0826903596e30f5396
Ali Aizad <aliaizad72@gmail.com>
2024-12-04 14:21:33 +0800

    Created Publisher model

---
+++ app/models/publisher.rb
@@ -0,0 +1,23 @@
+class Publisher < ApplicationRecord
+  validates :name, presence: true
+  validates :rss_url, presence: true, format: { with: URI.regexp(%w[http https]), message: "Only allow links with http/https" }
+
+  private
+
+  def rss_url_must_return_xml(url)
+    begin
+      response = URI.open(url)
+      content_type = response.meta["content-type"]&.split("; ")&.first
+      valid_content_types = %w[application/rss+xml application/atom+xml application/xml text/xml]
+      unless valid_content_types.include?(content_type)
+        errors.add :rss_url, :not_xml, message: "URL must return a valid XML feed (RSS/Atom)"
+      end
+    rescue OpenURI::HTTPError => e
+      errors.add :rss_url, :http_error, message: "HTTP error occurred: #{e.message}"
+    rescue SocketError
+      errors.add :rss_url, :connection_error, message: "Could not connect to the URL"
+    rescue StandardError => e
+      errors.add :rss_url, :standard_error, message: "An error occurred: #{e.message}"
+    end
+  end
+end
